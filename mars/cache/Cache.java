7aa206f96459a5df56f16e76e5dedeea50aa6f54
LuizHen96 <luizhen.cs@gmail.com>
2018-09-10 16:04:51 -0400

    Classe Cache
    Criado a classe Cache, que irá representar a própria cache em si

---
+++ mars/cache/Cache.java
@@ -0,0 +1,28 @@
+package mars.cache;
+
+public class Cache {
+	
+	private int nBlocks;
+	private int blockSize;
+	private int setSize;
+	private String cacheStorage;
+	private String placementPolicy;
+	private String replacementPolicy;
+	
+	
+	public Cache(int nBlocks, int blockSize, int setSize, String cacheStorage, String placementPolicy,
+			String replacementPolicy) {
+		super();
+		this.nBlocks = nBlocks;
+		this.blockSize = blockSize;
+		this.setSize = setSize;
+		this.cacheStorage = cacheStorage;
+		this.placementPolicy = placementPolicy;
+		this.replacementPolicy = replacementPolicy;
+	}
+
+	
+	
+	
+
+}
