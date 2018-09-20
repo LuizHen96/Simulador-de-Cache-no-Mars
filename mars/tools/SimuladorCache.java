9a42bcc6bc0739bb4ce1738de39fc2550719f7a8
LuizHen96 <luizhen.cs@gmail.com>
2018-09-20 16:56:15 -0400

    Incluido o Controller na tools
    Incluido o Controller na tools e adicionado precariamente o sistema de endereçamento

--- mars/tools/SimuladorCache.java
+++ mars/tools/SimuladorCache.java
@@ -2,15 +2,23 @@ package mars.tools;
 
 
 
+import java.util.Observable;
+
 import javax.swing.JComponent;
 import mars.cache.*;
 import mars.cache.swing.PanelCache;
+import mars.mips.hardware.AccessNotice;
+import mars.mips.hardware.Memory;
+import mars.mips.hardware.MemoryAccessNotice;
 import mars.cache.swing.InterfaceCache;
 
 public class SimuladorCache extends AbstractMarsToolAndApplication {
 	private static String name = "Simulador Cache";
 	private static String version = "Versão 0.1 (Luiz Henrique)";
 	private static String heading = "Simulador Didático de Cache";
+	private Controller controller;
+	private int memoryAccessCount;
+	private InterfaceCache panel;
 	
 
 	protected SimuladorCache(String title, String heading) {
@@ -28,10 +36,25 @@ public class SimuladorCache extends AbstractMarsToolAndApplication {
 
 	@Override
 	protected JComponent buildMainDisplayArea() {
-		InterfaceCache panel = new InterfaceCache();
+		Controller controller = new Controller();
+		panel = new InterfaceCache();
 		
 		
 		return panel;
 	}
+	
+	protected void addAsObserver() {
+		addAsObserver(Memory.textBaseAddress, Memory.textLimitAddress);
+	}
+	
+	protected void processMIPSUpdate(Observable resource, AccessNotice accessNotice) {
+		 MemoryAccessNotice notice = (MemoryAccessNotice) accessNotice;
+		 memoryAccessCount ++;
+		 
+		 panel.updateAddress(notice.getAddress());
+		 	 
+		 
+	}
+	
 
 }
