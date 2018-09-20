ff6b7ee75b29063a655d0ec26d17d2c370c7e575
LuizHen96 <luizhen.cs@gmail.com>
2018-09-20 07:49:03 -0400

    Classe Controller
    Classe que irá realizar o controle das classes e do view

---
+++ mars/cache/Controller.java
@@ -0,0 +1,73 @@
+package mars.cache;
+
+public class Controller {
+	private String tipoCache;
+	private String mapeamento;
+	private String politicaSubstituicao;
+	private int blocosConjunto;
+	private int nBlocos;
+	private int nPalavrasBloco;
+	private int tempoAcessoCache;
+	private int tempoAcessoMemoria;
+	
+	
+	public Controller(String tipoCache, String mapeamento, String politicaSubstituicao, int blocosConjunto, int nBlocos,
+			int nPalavrasBloco, int tempoAcessoCache, int tempoAcessoMemoria) {
+		super();
+		this.tipoCache = tipoCache;
+		this.mapeamento = mapeamento;
+		this.politicaSubstituicao = politicaSubstituicao;
+		this.blocosConjunto = blocosConjunto;
+		this.nBlocos = nBlocos;
+		this.nPalavrasBloco = nPalavrasBloco;
+		this.tempoAcessoCache = tempoAcessoCache;
+		this.tempoAcessoMemoria = tempoAcessoMemoria;
+	}
+
+
+	public String getTipoCache() {
+		return tipoCache;
+	}
+
+
+	public String getMapeamento() {
+		return mapeamento;
+	}
+
+
+	public String getPoliticaSubstituicao() {
+		return politicaSubstituicao;
+	}
+
+
+	public int getBlocosConjunto() {
+		return blocosConjunto;
+	}
+
+
+	public int getnBlocos() {
+		return nBlocos;
+	}
+
+
+	public int getnPalavrasBloco() {
+		return nPalavrasBloco;
+	}
+
+
+	public int getTempoAcessoCache() {
+		return tempoAcessoCache;
+	}
+
+
+	public int getTempoAcessoMemoria() {
+		return tempoAcessoMemoria;
+	}
+	
+	
+	
+	
+	
+	
+
+}
