77ed80ee1c5ba427d0a4c59cf1393ddb3044126e
LuizHen96 <luizhen.cs@gmail.com>
2019-02-05 16:12:47 -0300

    Class Comparator
    Desenha um comparador (=)

---
+++ mars/cache/swing/staticcache/Comparator.java
@@ -0,0 +1,25 @@
+package mars.cache.swing.staticcache;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Comparator extends JPanel{
+	
+
+	
+	public static final void paintComponent(Graphics g, int x, int y) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g2.drawOval(x,y, 30, 30);
+		Font font = new Font("Serif", Font.PLAIN, 30);
+		g2.setFont(font);
+		g2.drawString("=", x + 7, y + 25);	
+		font = new Font("Serif", Font.PLAIN, 14);
+		g2.setFont(font);
+	}
+	
+	
+	
+}
