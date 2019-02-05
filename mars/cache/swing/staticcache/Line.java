a5ea52a86af4fec832fe91abf09bbfe614313f7c
LuizHen96 <luizhen.cs@gmail.com>
2019-02-05 16:13:40 -0300

    Class Line

---
+++ mars/cache/swing/staticcache/Line.java
@@ -0,0 +1,26 @@
+package mars.cache.swing.staticcache;
+//package mars.cache.swing;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Line extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public static final void paintComponent(Graphics g, int x1, int y1, int x2, int y2) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g2.drawOval(x1-1, y1-1, 3, 3);
+		g2.drawLine(x1, y1, x2, y2);
+	}
+	
+}
