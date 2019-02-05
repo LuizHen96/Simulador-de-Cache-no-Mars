a6b6adcf036e846eefd084a195785e8f3736a50c
LuizHen96 <luizhen.cs@gmail.com>
2019-02-05 16:14:44 -0300

    Class Name
    Desenha uma linha e um nome, utilizado para informar qual o barramento

---
+++ mars/cache/swing/staticcache/Name.java
@@ -0,0 +1,34 @@
+package mars.cache.swing.staticcache;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Name extends JPanel{
+
+	
+	public static final void paintComponent(Graphics g, String name, int x, int y, boolean left) {
+		Graphics2D g2 = (Graphics2D) g;
+	
+		if(!left)
+		{	
+			g2.drawLine(x-6, y+5, x+8, y-10);
+			g2.drawString(name, x + 15, y);
+		}
+		else {
+			g2.drawLine(x-6, y+5, x+8, y-10);
+			g2.drawString(name, x - 35, y);
+		}
+		
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
