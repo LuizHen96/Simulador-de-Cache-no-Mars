884b004fe560f5018b4a5088bd90a43f77986b54
LuizHen96 <luizhen.cs@gmail.com>
2019-02-05 16:10:28 -0300

    Class Block
    Classe que desenha um bloco

---
+++ mars/cache/swing/staticcache/Block.java
@@ -0,0 +1,77 @@
+package mars.cache.swing.staticcache;
+//package mars.cache.swing;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Block extends JPanel {
+	
+	
+	private Word[] words;
+	private int nWords;
+	private int x, y;
+	
+	
+	static final int HEIGHT_ROW = Word.HEIGHT_ROW;
+	static final int WITDTH_V = 20;
+	static final int WITDTH_M = 0;
+	static final int WITDTH_TAG = 50;
+	
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public Block(int n, int x, int y) {
+		this.x = x;
+		this.y = y;
+		
+		nWords = n;
+		words = new Word[n];
+		for(int i=0; i<n; i++)
+		{
+			words[i] = new Word((WITDTH_V + WITDTH_M + Block.WITDTH_TAG+(i*Word.WITDTH_WORD))+x, y, i);
+		}
+		
+		
+		
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.black);
+		
+		
+		g2.drawRect(x, y, WITDTH_V, HEIGHT_ROW);
+		g2.drawRect(x+WITDTH_V, y, WITDTH_M, HEIGHT_ROW);
+		g2.drawRect(x+WITDTH_V+WITDTH_M, y, WITDTH_TAG, HEIGHT_ROW);
+		
+		
+		Font font = new Font("Serif", Font.PLAIN, 14);
+		g2.setFont(font);
+		//g2.drawString("V", x+4, y+HEIGHT_ROW-4);
+		//g2.drawString("M", x+4+WITDTH_V, y+HEIGHT_ROW-4);
+		//g2.drawString("TAG", x+4+WITDTH_V+WITDTH_M, y+HEIGHT_ROW-4);
+		
+		
+
+
+		for(int i =0; i<nWords; i++)
+		{
+			words[i].paintComponent(g);
+		}
+		
+		this.setBounds(x, y, (nWords*Word.WITDTH_WORD)+WITDTH_V + WITDTH_M + WITDTH_TAG, Word.HEIGHT_ROW);
+
+	}
+
+}
